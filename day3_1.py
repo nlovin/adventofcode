a071461f48839bfbde5cba41ba4e3db0fa2d9b45
Nathan Lovin <natelovin@gmail.com>
2024-12-06 19:56:45 -0500

    day 3, part 1 solution and data

---
+++ day3_1.py
@@ -0,0 +1,22 @@
+import re
+
+with open('data/day3.txt', 'r') as file:
+    memory = file.read()
+
+
+# Regular expression to match mul(x,y) where x and y are numbers
+matches = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', memory)
+
+pairs = []
+
+for match in matches:
+    x = int(match[0])
+    y = int(match[1])
+    pairs.append([x,y])
+
+total = 0
+
+for pair in pairs:
+    total += pair[0] * pair[1]
+
+print(total)
